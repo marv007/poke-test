3662d25308ecf97776c88a8db96ee5304a412e31
marv007 <marvintorres2013@gmail.com>
2025-01-24 10:48:47 -0600

    feat: add trainer page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TrainerComponent } from './pages/trainer/trainer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TrainerComponent },
+];
---
+++ src/app/pages/trainer/trainer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-trainer',
+  imports: [MatIcon],
+  templateUrl: './trainer.component.html',
+  styleUrl: './trainer.component.scss'
+})
+export class TrainerComponent {
+
+}
