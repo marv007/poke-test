625ec11e52be2fe6bd30a940816e5ac642b1010e
marv007 <marvintorres2013@gmail.com>
2025-01-24 17:10:48 -0600

    feat: add trainer page

--- src/app/pages/trainer/trainer.component.ts
+++ src/app/pages/trainer/trainer.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import { TrainerFormComponent } from "../../components/trainer-form/trainer-form.component";
+import { Trainer } from '../../models/trainer.model';
+import { TrainerPhotoComponent } from '../../components/trainer-photo/trainer-photo.component';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TrainerService } from '../../services/trainer.service';
 
 @Component({
   selector: 'app-trainer',
-  imports: [MatIcon],
+  imports: [MatIcon, TrainerFormComponent, TrainerPhotoComponent],
   templateUrl: './trainer.component.html',
   styleUrl: './trainer.component.scss'
 })
 export class TrainerComponent {
+  trainerPhoto: string | null = null;
+  constructor(
+    private trainerService: TrainerService,
+    private toastr: ToastrService,
+    private router: Router
+  ){}
+
+  saveTrainerData(formData: Trainer): void {
+    if (!this.trainerPhoto) {
+      this.toastr.error('Por favor, seleccione una foto');
+      return;
+    }
+
+    const fullTrainerData = {
+      ...formData,
+      isLogged: false,
+      photo: this.trainerPhoto,
+    };
+
+    this.trainerService.createTrainer(fullTrainerData);
+
+    //this.router.navigate(['/pokemon-selection']);
+  }
+
+  handlePhotoUpload(photo: string | null): void {
+    this.trainerPhoto = photo;
+  }
 
 }
+
+
