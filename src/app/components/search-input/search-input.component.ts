77a444d5681691cff8c3657f59ef39bfc000281e
marv007 <marvintorres2013@gmail.com>
2025-01-24 12:21:24 -0600

    feat: add search input component

---
+++ src/app/components/search-input/search-input.component.ts
@@ -0,0 +1,69 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ControlValueAccessor } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-input',
+  imports: [CommonModule],
+  templateUrl: './search-input.component.html',
+  styleUrl: './search-input.component.scss'
+})
+export class SearchInputComponent implements ControlValueAccessor {
+  @Input() options: string[] = [];
+  @Output() optionSelected = new EventEmitter<string>();
+
+  filteredOptions: string[] = [];
+  showOptions = false;
+  searchQuery = '';
+  selectedOption: string | null = null;
+
+  onChange: (value: string) => void = () => {};
+  onTouched: () => void = () => {};
+
+  constructor() {
+    this.filteredOptions = [...this.options];
+  }
+
+  onFocus() {
+    this.showOptions = true;
+    this.filterOptions();
+  }
+
+  onInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const query = inputElement.value;
+    this.searchQuery = query;
+    this.filterOptions();
+  }
+
+  filterOptions() {
+    this.filteredOptions = this.options.filter((option) =>
+      option.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
+
+  selectOption(option: string) {
+    this.selectedOption = option;
+    this.searchQuery = option;
+    this.showOptions = false;
+    this.onChange(option);
+    this.optionSelected.emit(option);
+    this.showOptions = false;
+    this.onTouched();
+  }
+
+  writeValue(value: string): void {
+    if (value) {
+      this.selectedOption = value;
+      this.searchQuery = value;
+    }
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+}
