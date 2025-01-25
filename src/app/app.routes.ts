7d27115f16479c6b4c509d7b7899ff970e5393ed
marv007 <marvintorres2013@gmail.com>
2025-01-24 23:20:29 -0600

    feat: add pokemon selection page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { TrainerComponent } from './pages/trainer/trainer.component';
+import { PokemonSelectionComponent } from './pages/pokemon-selection/pokemon-selection.component';
 
 export const routes: Routes = [
   { path: '', component: TrainerComponent },
+  {
+    path: 'pokemon-selection', component: PokemonSelectionComponent
+  }
 ];
