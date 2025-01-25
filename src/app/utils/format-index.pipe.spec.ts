e6bcaafa4d3e9281af68fe6cd73c0e49818d657e
marv007 <marvintorres2013@gmail.com>
2025-01-24 22:26:36 -0600

    feat: add index pipe

---
+++ src/app/utils/format-index.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormatIndexPipe } from './format-index.pipe';
+
+describe('FormatIndexPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatIndexPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
