1cab5f862251850a42c3f10be625567664717cea
marv007 <marvintorres2013@gmail.com>
2025-01-25 22:31:25 -0600

    feat: register the route of profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { TrainerComponent } from './pages/trainer/trainer.component';
 import { PokemonSelectionComponent } from './pages/pokemon-selection/pokemon-selection.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
   { path: '', component: TrainerComponent },
   {
     path: 'pokemon-selection', component: PokemonSelectionComponent
+  },
+  {
+    path: 'profile', component: ProfileComponent
   }
 ];
