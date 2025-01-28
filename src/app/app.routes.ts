2c73e82f1ccc485c175dfd764580a22bbea3adc1
marv007 <marvintorres2013@gmail.com>
2025-01-27 21:28:32 -0600

    feat(chore): add trainer guard to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,27 @@ import { Routes } from '@angular/router';
 import { TrainerComponent } from './pages/trainer/trainer.component';
 import { PokemonSelectionComponent } from './pages/pokemon-selection/pokemon-selection.component';
 import { ProfileComponent } from './pages/profile/profile.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { trainerGuard } from './guards/trainer.guard';
 
 export const routes: Routes = [
   { path: '', component: TrainerComponent },
   {
-    path: 'pokemon-selection', component: PokemonSelectionComponent
+    path: 'pokemon-selection',
+    component: PokemonSelectionComponent,
+    canActivate: [trainerGuard],
   },
   {
-    path: 'profile', component: ProfileComponent
-  }
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [trainerGuard],
+  },
+  {
+    path: 'error',
+    component: ErrorComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/error',
+  },
 ];
